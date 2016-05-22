2e938372925e3cc7af567a53aafeeae8b6c97348
Cyprien Autexier <cyprien.autexier@gmail.com>
2016-05-22 11:05:03 +0200

    .net core rc2 support

---
+++ csharp/PhoneNumbers-core/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("libphonenumber-csharp-portable")]
+[assembly: AssemblyDescription("Portable library port of libphonenumber")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Erez A. Korn (esemobilesoftware)")]
+[assembly: AssemblyProduct("libphonenumber-csharp-portable")]
+[assembly: AssemblyCopyright("(C) 2013 Erez A. Korn")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d42d3a3e-e8b7-41d6-89b4-b83c29c1ac6b")]
